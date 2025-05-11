76adb4b2b6b50c748e7e6c032e305cfbe4bdae85
fatima1414 <shaikhfatima5260@gmai.com>
2025-05-11 11:26:07 +0530

    code

--- Function_concept/script.js
+++ Function_concept/script.js
@@ -7,10 +7,10 @@ function show() {
 }
 
 //   4) Immediate Invoke Function Expression(IIFE || efi)
-( () => {
+(() => {
   console.log("IIFE Function");
-  
-}   ) ()
+
+})()
 
 // 2) Annonymous Function
 var show2 = function () {
@@ -26,33 +26,33 @@ show3();
 
 
 // FUNCTION CONCEPT
-function add (a,b){
-    console.log(`addition of ${a} & ${b} = ${a + b }`);
+function add(a, b) {
+  console.log(`addition of ${a} & ${b} = ${a + b}`);
 }
-add(5,6)
-add (7,1)
+add(5, 6)
+add(7, 1)
 
 
 // Without parameter
-function addi (){
-    var a = 40;
-    var b = 55 ;
-    console.log("addition =", (a + b));    
+function addi() {
+  var a = 40;
+  var b = 55;
+  console.log("addition =", (a + b));
 }
-addi(5,6)
-addi (7,1)
+addi(5, 6)
+addi(7, 1)
 
 // with parameter
-function sum (c,d){
-    console.log("addition =", (c + d));    
+function sum(c, d) {
+  console.log("addition =", (c + d));
 }
-sum (4,5)
-sum (4,5)
+sum(4, 5)
+sum(4, 5)
 
 // with return argument
-function calc (e,f){
-   return e + f   
+function calc(e, f) {
+  return e + f
 }
-console.log("addition =", calc (45, 4)); 
-const output = calc(2,3)
+console.log("addition =", calc(45, 4));
+const output = calc(2, 3)
 console.log("sum =", output);
